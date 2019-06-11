8d135c3525b5c74c8d70383b48bd72a899b9dc58
Max Kostinevich <max.kostinevich@gmail.com>
2019-06-11 14:16:22 +0300

    Enable auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Force HTTPS connection
+        \URL::forceScheme('https');
     }
 }
