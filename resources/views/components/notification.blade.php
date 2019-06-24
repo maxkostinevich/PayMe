829fe6263662361c1ac321976e8eed686ecdbe9b
Max Kostinevich <max.kostinevich@gmail.com>
2019-06-25 00:49:52 +0300

    Update notification

--- resources/views/components/notification.blade.php
+++ resources/views/components/notification.blade.php
@@ -1,3 +1,9 @@
+@if(!auth()->user()->stripe_account_id)
+    <div class="alert alert-warning" role="alert">
+        Please, <a href="{{ route('settings.edit') }}">connect</a> your Stripe Account
+    </div>
+@endif
+
 @if ($errors->any())
     <div class="alert alert-danger" role="alert">
         <div class="font-weight-bold">Oops! Please, fix the following errors:</div>
