246c2d581d250b33deb03b48f9556231e00d539d
Max Kostinevich <max.kostinevich@gmail.com>
2019-06-24 16:50:19 +0300

    Add currencylayer api key

---
+++ config/currencylayer.php
@@ -0,0 +1,9 @@
+<?php
+
+// Currencylayer Settings
+return [
+    // Currencylayer API Key
+    // Obtain your free key at https://currencylayer.com
+    'api' => env('CURRENCYLAYER_API_KEY', ''),
+
+];
